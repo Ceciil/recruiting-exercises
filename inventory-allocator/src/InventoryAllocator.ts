c9cc596f18530c86a13aa2903c5537f3662229c0
Angela Lee <rubii.kun@gmail.com>
2020-08-25 12:35:06 -0700

    Add docs and fixed sort functionality

--- inventory-allocator/src/InventoryAllocator.ts
+++ inventory-allocator/src/InventoryAllocator.ts
@@ -2,23 +2,29 @@ import { Order, Warehouses, Shipment } from './Types';
 
 type ShipmentIndex = { [warehouse: string]: { [name: string]: number } };
 
-export class InventoryAllocator {
+/**
+ * Inventory Allocator class has a method that returns a list of warehouses that can fulfill the order
+ * @param {Order} order - Set of items in an order
+ * @param {Warehouses} warehouses - Array of warehouses
+ * @return {Array} - The list of warehouses that fulfill the order
+ */
 
-  public allocate(
-    order: Order, 
-    warehouses: Warehouses
-  ): Shipment {
+export class InventoryAllocator {
+  public allocate(order: Order, warehouses: Warehouses): Shipment {
     const shipmentIndex: ShipmentIndex = {};
     const fulfilled: { [item: string]: number } = {};
 
-    // greedily accumulate items from warehouses when available
+    // Greedily accumulate items from warehouses when available
     for (let [item, count] of Object.entries(order)) {
       for (let { name, inventory } of warehouses) {
-        const fulfilledItem = fulfilled[item] || 0
+        const fulfilledItem = fulfilled[item] || 0;
+
         if (!inventory[item]) break;
+
         if (fulfilledItem < count) {
           const needsFulfilled = count - fulfilledItem;
           const canBeFulfilled = Math.min(inventory[item], needsFulfilled);
+
           if (canBeFulfilled > 0) {
             fulfilled[item] = fulfilled[item] || 0;
             shipmentIndex[name] = shipmentIndex[name] || {};
@@ -30,22 +36,26 @@ export class InventoryAllocator {
       }
     }
 
-    // check that values in fulfilled is same as in order
+    // Check that values in fulfilled is same as in order
     for (let key of [...Object.keys(order), ...Object.keys(fulfilled)]) {
       if (order[key] !== fulfilled[key]) {
         return [];
       }
     }
 
-    // convert shipmentIndex to shipment
-    const shipment = []
+    // Convert shipmentIndex to shipment
+    const shipment = [];
     for (let [warehouse, fulfillment] of Object.entries(shipmentIndex)) {
       shipment.push({ [warehouse]: fulfillment });
     }
 
-    // sort shipment to return warehouse in alphabetical order
-    shipment.sort((a, b) => Object.keys(a)[0].charCodeAt(0) - Object.keys(b)[0].charCodeAt(0));
+    // Sort shipment to return warehouse in alphabetical order
+    shipment.sort((a, b) => {
+      const nameA = Object.keys(a)[0].toUpperCase();
+      const nameB = Object.keys(b)[0].toUpperCase();
+      return nameA.localeCompare(nameB);
+    });
+
     return shipment;
   }
-
 }
